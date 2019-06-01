dbfcc01d81e3ad8166416f1124309f56f3381e44
gaurav8354 <gaurav18.2013@gmail.com>
2019-06-02 05:13:29 +0530

    Orderdetail fixed+image added

--- app/src/main/java/com/instadp/profilepicture/finalfoodappserver/OrderDetail.java
+++ app/src/main/java/com/instadp/profilepicture/finalfoodappserver/OrderDetail.java
@@ -5,26 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.instadp.profilepicture.finalfoodappserver.Common.Common;
 import com.instadp.profilepicture.finalfoodappserver.Interface.ItemOnClickListner;
 import com.instadp.profilepicture.finalfoodappserver.Model.Food;
 import com.instadp.profilepicture.finalfoodappserver.Model.Order;
 import com.instadp.profilepicture.finalfoodappserver.Model.Request;
+import com.instadp.profilepicture.finalfoodappserver.Model.Request1;
 import com.instadp.profilepicture.finalfoodappserver.ViewHolder.OrderDetailViewHolder;
 import com.instadp.profilepicture.finalfoodappserver.ViewHolder.OrderViewHolder;
+import com.squareup.picasso.Picasso;
 
 public class OrderDetail extends AppCompatActivity {
     public RecyclerView recyclerView;
     public RecyclerView.LayoutManager  layoutManager;
+    TextView prttl,prname,prnum,praltnum;
     FirebaseRecyclerAdapter<Order,OrderDetailViewHolder> adapter;
     FirebaseDatabase db;
-    DatabaseReference requests;
+    DatabaseReference requests,rq1;
 Intent i;
 String orderid="";
     @Override
@@ -32,16 +40,76 @@ String orderid="";
         super.onCreate(savedInstanceState);
         i=getIntent();
         setContentView(R.layout.activity_order_detail);
+//        getActionBar().setTitle(i.getStringExtra("ordid"));
+        getSupportActionBar().setTitle(i.getStringExtra("ordid"));
         db=FirebaseDatabase.getInstance();
         requests=db.getReference("Requests").child(i.getStringExtra("ordid")).child("food");
+        rq1=db.getReference("Request").child(i.getStringExtra("ordid"));
         recyclerView= (RecyclerView) findViewById(R.id.order_detail_recycleview);
         recyclerView.setHasFixedSize(true);
         layoutManager=new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
          loadOrder();
-
-
+         loadDetails();
     }
+
+    private void loadDetails() {
+        praltnum= (TextView) findViewById(R.id.order_detail_main_altnumber);
+        prname= (TextView) findViewById(R.id.order_detail_main_name);
+        prttl= (TextView) findViewById(R.id.order_detail_main_total);
+        prnum= (TextView) findViewById(R.id.order_detail_main_number);
+        final DatabaseReference zonesRef1 = FirebaseDatabase.getInstance().getReference("Requests").child(i.getStringExtra("ordid")).child("address");
+        DatabaseReference zonesRef2 = FirebaseDatabase.getInstance().getReference("Requests").child(i.getStringExtra("ordid")).child("name");
+        DatabaseReference zonesRef3 = FirebaseDatabase.getInstance().getReference("Requests").child(i.getStringExtra("ordid")).child("phone");
+        DatabaseReference zonesRef4 = FirebaseDatabase.getInstance().getReference("Requests").child(i.getStringExtra("ordid")).child("total");
+
+        zonesRef1.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //viewHolder.foodtotal.setText(dataSnapshot.getValue().toString());
+                praltnum.setText("Alt-num : "+dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });zonesRef4.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //viewHolder.foodtotal.setText(dataSnapshot.getValue().toString());
+                prttl.setText("Total : ₹"+dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });zonesRef2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //viewHolder.foodtotal.setText(dataSnapshot.getValue().toString());
+                prname.setText("Name : "+dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });zonesRef3.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //viewHolder.foodtotal.setText(dataSnapshot.getValue().toString());
+             prnum.setText("Number: "+dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     private void loadOrder() {
         adapter=new FirebaseRecyclerAdapter<Order, OrderDetailViewHolder>(
                 Order.class,
@@ -52,12 +120,26 @@ String orderid="";
 
 
             @Override
-            protected void populateViewHolder(OrderDetailViewHolder viewHolder, Order model, int position) {
-                viewHolder.foodprice.setText(model.getPrice());
+            protected void populateViewHolder(final OrderDetailViewHolder viewHolder, Order model, int position) {
+                viewHolder.foodprice.setText("Price: ₹"+model.getPrice());
                 viewHolder.foodname.setText(model.getProduceName());
-                viewHolder.foodquantity.setText(model.getQuantity());
+                viewHolder.foodquantity.setText("Quantity : "+model.getQuantity());
                 String total=Integer.parseInt(model.getQuantity())*Integer.parseInt(model.getPrice())+"";
-                viewHolder.foodtotal.setText(total);
+                viewHolder.foodtotal.setText("Total Price: ₹"+total);
+                DatabaseReference zonesRef = FirebaseDatabase.getInstance().getReference("Food").child(model.getProductId()).child("image");
+                zonesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //viewHolder.foodtotal.setText(dataSnapshot.getValue().toString());
+
+                        Picasso.with(getBaseContext()).load(dataSnapshot.getValue().toString()).into(viewHolder.fooddetailimage);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
 
 
             }
@@ -67,4 +149,5 @@ String orderid="";
         adapter.notifyDataSetChanged();
         recyclerView.setAdapter(adapter);
     }
+
 }
--- app/src/main/java/com/instadp/profilepicture/finalfoodappserver/ViewHolder/OrderDetailViewHolder.java
+++ app/src/main/java/com/instadp/profilepicture/finalfoodappserver/ViewHolder/OrderDetailViewHolder.java
@@ -3,6 +3,7 @@ package com.instadp.profilepicture.finalfoodappserver.ViewHolder;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.instadp.profilepicture.finalfoodappserver.R;
@@ -10,12 +11,14 @@ import com.instadp.profilepicture.finalfoodappserver.R;
 public class OrderDetailViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,View.OnCreateContextMenuListener {
 
     public TextView foodname,foodprice,foodquantity,foodtotal;
+    public ImageView fooddetailimage;
     public OrderDetailViewHolder(View itemView) {
         super(itemView);
         foodname= (TextView) itemView.findViewById(R.id.order_detail_product_name);
         foodprice= (TextView) itemView.findViewById(R.id.order_detail_product_price);
         foodquantity= (TextView) itemView.findViewById(R.id.order_detail_product_quantity);
         foodtotal= (TextView) itemView.findViewById(R.id.order_detail_product_total);
+        fooddetailimage= (ImageView) itemView.findViewById(R.id.order_detail_image);
     }
 
     @Override
